d9d5bb10bddf5769c470b236ba996a2cd80cd6ec
Ramy Moussa <Ramy.Yousef@outlook.com>
2021-09-20 16:42:09 +0200

    Add the API

---
+++ SudoRoux.ContactUs.API/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SudoRoux.ContactUs.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly IWebHostEnvironment _env;
+
+        public HomeController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpPost]
+        public void Post(Domain.ContactUs contactUs)
+        {
+            string jsonString = JsonSerializer.Serialize(contactUs);
+            var path = Path.Combine(_env.ContentRootPath, "contactUs.json");
+            System.IO.File.WriteAllText(path, jsonString);
+        }
+    }
+}
