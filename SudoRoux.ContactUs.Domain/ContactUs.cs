81ad8bca55e2108d96e57df2d474b7d91e063622
Ramy Moussa <Ramy.Yousef@outlook.com>
2021-09-20 15:10:59 +0200

    Add Entity with Validations

---
+++ SudoRoux.ContactUs.Domain/ContactUs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SudoRoux.ContactUs.Domain
+{
+
+    public class ContactUs
+    {
+        private const int minimumAge = 10;
+        private const int maximumAge = 120;
+
+        [Required(AllowEmptyStrings = false)]
+        [MinLength(2)]
+        [MaxLength(20)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Alphabets only allowed")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [MinLength(2)]
+        [MaxLength(20)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Alphabets only allowed")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        public bool Subscribed { get; set; }
+
+        [DataType(DataType.Date)]
+        [PastDate(minimumAge, maximumAge)]
+        [Display(Name = "Date Of Birth")]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
+
+
---
+++ SudoRoux.ContactUs.Domain/PastDateAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudoRoux.ContactUs.Domain
+{
+    class PastDateAttribute : RangeAttribute
+    {
+        public PastDateAttribute(int minimumPastYears, int maximumPastYears)
+               : base(typeof(DateTime),  DateTime.Now.AddYears(-maximumPastYears).ToString(), DateTime.Now.AddYears(-minimumPastYears).ToString()) 
+        { }
+    }
+}
